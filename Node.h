69017777c14b1971a22ee8e317385bbe594d4f82
trong <leafylotus@gmail.com>
2023-10-16 13:58:55 -0700

    Added node class

---
+++ Node.h
@@ -0,0 +1,24 @@
+//
+// Created by T D  on 10/16/23.
+//
+
+#ifndef SINGLYLINKEDLISTSTRING_NODE_H
+#define SINGLYLINKEDLISTSTRING_NODE_H
+
+
+class Node {
+public:
+
+public:
+    Node(char data) { m_data = data; }
+    void setData(char data){  m_data = data; }
+    char getData() { return m_data; }
+    void setPrevious(Node* next) { m_next = next; }
+    Node* getNext() { return m_next; } //
+private:
+    char m_data;
+    Node* m_next;
+};
+
+
+#endif //SINGLYLINKEDLISTSTRING_NODE_H
--- SLLString.h
+++ SLLString.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include "Node.h"
 
 class SLLString
 {
