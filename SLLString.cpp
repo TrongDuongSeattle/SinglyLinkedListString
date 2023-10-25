bd4f2f361c85384c833172741d24a11385492674
Trong <you@example.com>
2023-10-25 14:28:05 -0700

    Final Draft

--- SLLString.cpp
+++ SLLString.cpp
@@ -7,7 +7,7 @@ SLLString::SLLString(){
     m_pTail = nullptr;
 }
 /**
- * Copy Constructor
+ * Constructs an SLLString from a string literal to SLLString
  * @param other
  */
 SLLString::SLLString(const std::string& other){
@@ -21,17 +21,13 @@ SLLString::SLLString(const std::string& other){
     }
 }
 /**
- *Heavily inspired by textbook
- *
- * take the other pointers assign head and tail to other pointer
- * then set other pointer to null..
- * Traverse, loop and duplicate: looplicate. Dupli-Loop.;
+ * Constructs a new SLLString object from other SLLString object
  * @param other
  */
 SLLString::SLLString(const SLLString& other){
     size = other.size;
     if (other.m_pHead == nullptr)
-        m_pHead = nullptr;
+        SLLString();
     else {
         Node* otherHead = other.m_pHead;
         m_pHead = m_pTail = otherHead;
@@ -48,23 +44,89 @@ SLLString::SLLString(const SLLString& other){
 
 }
 /**
- * iterate list, delete, set head & tail to null
  * Destructor
  */
 SLLString::~SLLString() {
     clear();
 }
 /**
- * once you have the data sorted, searching becomes trivial;
- * @param substring
+ * Copies contents of other SLLString to this SLLString.
+ * @param other
  * @return
+ */
+SLLString& SLLString::operator=(const SLLString& other) {
+    if (this == &other)
+        return *this;
+    clear();
+    size = other.size;
+    Node* otherHead = other.m_pHead;
+    m_pHead = new Node(otherHead->getData());
+    m_pHead->setNext(otherHead->getNext());
+    m_pTail = m_pHead;
+
+    otherHead = otherHead->getNext();
+    while (otherHead != nullptr) {
+        char ch = otherHead->getData();
+        Node *newNodePtr = new Node(ch);
+        m_pTail->setNext(newNodePtr);
+        m_pTail = m_pTail->getNext();
+        otherHead = otherHead->getNext();
+    }
+    return *this;
+}
+/**
+ * Appends given SLLString to the end of this SLLString.
+ * @param other
+ * @return Address this SLLString
+ */
+SLLString& SLLString::operator+= (const SLLString& other) {
+    this->size += other.size;
+    Node* otherHead = other.m_pHead;
+    while(otherHead != nullptr) {
+        this->m_pTail->setNext(new Node(otherHead->getData()));
+        this->m_pTail = m_pTail->getNext();
+        otherHead = otherHead->getNext();
+    }
+    return *this;
+}
+/**
+ * Allows us to print with << instead of calling a print function.
+ */
+ std::ostream& operator<<(std::ostream& os, const SLLString& other) {
+    int index = 0;
+    Node* temp = other.m_pHead;
+    while (temp != 0) {
+        os <<  temp->getData();
+        temp = temp->getNext();
+        index++;
+    }
+    return os;
+};
+/**
+ * Get character at index n
+ * @param substring
+ * @return Address of character
+ */
+char& SLLString::operator[](const int n){
+    Node* temp = m_pHead;
+    for (int i = 0; n; i++) {
+        if (i == n) {
+            return temp->getData();
+        }
+        temp = temp->getNext();
+    }
+
+}
+/**
+ * Finds the index of the first occurrence of substring in the current string.
+ * @param substring
+ * @return Index of first letter in substring. Returns -1 if not found.
  */
 int SLLString::findSubstring(const std::string substring){
     int index = 0;
     int count = 0;
     Node* thisNode = m_pHead;
     bool found = false;
-
    while (!found && thisNode != nullptr) {
        if (thisNode->getData() == substring[index]) {
            index++;
@@ -78,8 +140,10 @@ int SLLString::findSubstring(const std::string substring){
        return count - index;
     return -1;
 }
-// Erase all occurrences of character c from the current string
-// iterate, erase
+/**
+ * Erase all occurrences of character c from the current string
+ * @param c
+ */
 void SLLString::erase(char c) {
     Node* curr = m_pHead;
     while (curr->getNext()->getNext() != nullptr) {
@@ -97,7 +161,9 @@ void SLLString::erase(char c) {
         return;
     }
 }
-//my fx's
+/**
+ * Deletes entire SLLString.
+ */
 void SLLString::clear() {
     if (size != 0) {
         Node *killer = m_pHead;
--- SLLString.h
+++ SLLString.h
@@ -9,111 +9,17 @@ class SLLString
 {
     public:
 	SLLString();
-	SLLString(const std::string& other); //convertor
-	SLLString(const SLLString& other); //copy constr
+	SLLString(const std::string& other);
+	SLLString(const SLLString& other);
 	~SLLString();
-    /**
-     * thanks Nathaniel
-     * Assignment:
-     *  Base obj1(5); //calls Base class constructor
-     *  Base obj2; //calls Base class default constructor
-     *  obj2 = obj1; //calls assignment operator
-     *
-     *  Copy Constructor:
-     *  Base obj1(5);
-     *  Base obj2 = obj1; //calls copy constructor
-     *
-     * @param other
-     * @return
-     */
-	SLLString& operator=(const SLLString& other) { //returns an adress
-        if (this == &other)
-            return *this;
-        clear();//this is the distinguishing line.
-        size = other.size;
-        Node* otherHead = other.m_pHead;
-        m_pHead = new Node(otherHead->getData()); //creating first node.
-        m_pHead->setNext(otherHead->getNext()); //this is the head, mess with this and lose everything
-        m_pTail = m_pHead;
-        //copy remaining nodes
-        //Node* new_mpHead = new Node(); // temp pointer
-        otherHead = otherHead->getNext(); // advancing pointer..
-        while (otherHead != nullptr) {
-            // get next item from original chain
-            char ch = otherHead->getData();
-
-            // create a new node containing the next item
-            Node *newNodePtr = new Node(ch);
-
-            // link new node to end of new chain
-            m_pTail->setNext(newNodePtr);
-
-            // Advance ptrs
-            m_pTail = m_pTail->getNext();
-            otherHead = otherHead->getNext();
-        }
-        return *this;
-
-    }
-    /**
-     * string concat
-     * get to the end, stick it on.
-     * @param other
-     * @return
-     */
-	SLLString& operator+= (const SLLString& other) { //modifying current list
-        this->size += other.size;
-        Node* otherHead = other.m_pHead;
-        while(otherHead != nullptr) {
-            //dnew Node(otherHead->getData());
-            this->m_pTail->setNext(new Node(otherHead->getData())); //new node returns a node ptr, set next tkes a node ptr
-
-            this->m_pTail = m_pTail->getNext();
-            otherHead = otherHead->getNext();
-        }
-        return *this;
-    }
-	friend std::ostream& operator<<(std::ostream& os, const SLLString& other) {
-        int index = 0;
-        Node* temp = other.m_pHead;
-        while (temp != 0) {
-            os << index << "\t" << temp->getData() << std::endl;
-            temp = temp->getNext();
-            index++;
-        }
-        return os;
-    };
-
-	// Get character at index n
-	char& operator[](const int n){
-        Node* temp = m_pHead;
-        for (int i = 0; n; i++) {
-            if (i == n) {
-                return temp->getData();
-            }
-            temp = temp->getNext();
-        }
-
-    }
-
-
-
-	// Get the length of this string
+	SLLString& operator=(const SLLString& other);
+	SLLString& operator+= (const SLLString& other);
+	friend std::ostream& operator<<(std::ostream& os, const SLLString& other);
+    char& operator[](const int n);
 	int length() { return size; };
-
-	// Find the index of the first occurrence of substring in the current string.
-	// Returns - 1 if not found
 	int findSubstring(const std::string substring);
-
-	// Erase all occurrences of character c from the current string
-    // iterate, erase
-	void erase(char c);
-
+    void erase(char c);
     void clear();
-
-
-
-
 private:
 	Node* m_pHead;
 	Node* m_pTail;
